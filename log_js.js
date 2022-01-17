e8653d2c0bb4d3d1054bc2397d947c10eb0681fe
Zoe Zhang <zoe.zhang@asml.com>
2022-01-17 17:48:22 +0800

    html and css  note update

---
+++ log_js.js
@@ -0,0 +1,11 @@
+/* Javascript code is downloaded and keep talking 
+to the server to get the update from the server, and 
+js accordingly modify the DOM(document object model),
+can think DOM as the tree in memory that represent html.
+
+exist in head/body of the page or a seperate file too.
+
+<script></script>
+
+
+*/
