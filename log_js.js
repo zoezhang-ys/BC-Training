cef18f51160d4368f768c28927b2d444b5abe7e9
Zoe Zhang <zoe.zhang@asml.com>
2022-01-18 16:24:22 +0800

    Javascript fundamental

--- log_js.js
+++ log_js.js
@@ -5,7 +5,97 @@ can think DOM as the tree in memory that represent html.
 
 exist in head/body of the page or a seperate file too.
 
-<script></script>
-
+<script>
+</script>
 
 */
+
+/* ====================== JS Notes ====================== 
+
+1.0 Variables: numbers, string, bool, null, undefined
+
+var a = 10;
+var b = 5;
+
+b++;
+var c = ( a + b / 2)% 3;
+
+var my_str = 'hello.'
+var str_num = "5";
+var changed = Number(str_num);
+
+Using var to declear variable, the value won't lose.
+Cannot re-declare a variable using let or const.
+
+
+
+====================================================== */
+
+// Action
+
+// Define the function to be used in the HTML 
+function greet(){
+
+    // get the name from the onsubmit element,
+    // which has an id of "name", grab the actual typed in
+    let submitted_name = document.querySelector("#name").value;
+
+    alert('hello there ' + submitted_name);
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelector('#say_hello').addEventListener('submit', greet);
+});
+
+
+/* For the top 2 block, they can be replaced as followed: 
+
+document.querySelector('form').addEventListener('submit', function (e){
+
+    let submitted_name = document.querySelector("#name").value;
+    alert('hello there ' + submitted_name);
+    e.preventDefault();
+});
+*/
+
+
+//var body = document.querySelector('body');
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelector('#red').addEventListener('click', function() {
+        body = document.querySelector('body');
+        body.style.backgroundColor = 'red';
+    });
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelector('#green').addEventListener('click', function() {
+        body = document.querySelector('body');
+        body.style.backgroundColor = 'green';
+    });
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelector('#blue').addEventListener('click', function() {
+        body = document.querySelector('body');
+        body.style.backgroundColor = 'blue';
+    });
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelector('#clear').addEventListener('click', function() {
+        body = document.querySelector('body');
+        body.style.backgroundColor = 'white';
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
