d29e417cab08a66a5f65a98ff83bd945b4088655
Zoe Zhang <zoe.zhang@asml.com>
2022-03-19 15:10:44 +0800

    R notes

---
+++ R/Data Science - R Basics/draft.R
@@ -0,0 +1,12 @@
+library(dslabs)
+data("murders")
+
+# the type of the object
+print(class(murders))
+
+# the structure of the object
+print(str("murders"))
+
+# the first 6 line 
+print(head(murders))
+
