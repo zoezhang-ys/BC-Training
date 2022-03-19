6f3207935956f73e18bf9e26874bc1da8d1b20b0
Zoe Zhang <zoe.zhang@asml.com>
2022-03-19 17:24:31 +0800

    R update

--- R/Data Science - R Basics/draft.R
+++ R/Data Science - R Basics/draft.R
@@ -5,8 +5,14 @@ data("murders")
 print(class(murders))
 
 # the structure of the object
+# state abb region population total
 print(str("murders"))
+
+# get the names of the columns
+print(names(murders))
 
 # the first 6 line 
 print(head(murders))
 
+# select a column
+print(murders$population)
